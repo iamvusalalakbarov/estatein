c85031331767a39d3fb9abc07346efa5e81bf32b
Vusal Alakbarov <vusal11010@gmail.com>
2024-04-28 13:07:46 +0400

    Extended custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,34 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        purple: {
+          60: "#703BF7",
+          65: "#8254F8",
+          70: "#946CF9",
+          75: "#A685FA",
+          90: "#DBCEFD",
+          95: "#EDE7FE",
+          97: "#F4F0FE",
+          99: "#FBFAFF",
+        },
+        white: {
+          90: "#E4E4E7",
+          95: "#F1F1F3",
+          97: "#F7F7F8",
+          99: "#FCFCFD",
+        },
+        gray: {
+          8: "#141414",
+          10: "#1A1A1A",
+          15: "#262626",
+          20: "#333333",
+          30: "#4D4D4D",
+          40: "#666666",
+          50: "#808080",
+          60: "#999999",
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
